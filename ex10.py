f86f384ab244c08e4f54a50b9101294c198943a6
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-02-19 07:55:37 -0300

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,4 @@
+C=float(input("Olá, me informe a temperatura em Celsius que eu transformarei em graus Fahrenheit!"))
+F =  C * 1.8 + 32;
+F_FORMATADO=round(F,2)
+print(f"{C} graus Celsius é equivalente a {F_FORMATADO} graus Fahrenheit")

df286b2907a7dfba226d48d4ab7c5573ce709c8c
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-02-19 07:48:32 -0300

    Create exercicio9.py

---
+++ exercicio9.py
@@ -0,0 +1,4 @@
+F=int(input("Olá, me informe a temperatura em Fahrenheit que eu transformarei em graus Celsius!"))
+C= (5 * ((F-32) / 9))
+C_FORMATADO=round(C,2)
+print(f"{F} graus Fahrenheit é equivalente a {C_FORMATADO} graus Celsius")

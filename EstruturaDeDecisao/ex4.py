97d4c79ea588d678700b4a46c6c82394894a53cf
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-02-21 00:17:04 -0300

    Update ex4.py

--- EstruturaDeDecisao/ex4.py
+++ EstruturaDeDecisao/ex4.py
@@ -1,4 +1,9 @@
 #Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-letra=input("Escreva uma letra")
-if letra=="a"or letra=="e" or letra=="i" or letra=="o" or letra=="u"
-    print()
+letra=input("Escreva uma letra:\n")
+if (ord(letra)>64 and ord(letra)<91) or (ord(letra)>96 and ord(letra)<123):
+    if letra=="a"or letra=="e" or letra=="i" or letra=="o" or letra=="u":
+        print(f"A letra {letra} é uma vogal.")
+    else:
+        print(f"A letra {letra} é uma consoante.")
+else:
+    print("Caractere inválido.")
